aebe89ac854918257e6ba539e9b6be5fc3ecc4ea
José Carlos Del Valle <30965492+jcval94@users.noreply.github.com>
2025-12-21 00:10:12 -0600

    Rename rule search helper to find_comb_dim_spaces

--- src/sheshe/__init__.py
+++ src/sheshe/__init__.py
@@ -3,6 +3,7 @@ from .subspace_scout import SubspaceScout
 from .modal_scout_ensemble import ModalScoutEnsemble
 from .region_interpretability import RegionInterpreter
 from .meta_optimization import random_search
+from .combiantions import find_comb_dim_spaces, plot_rule_metrics, select_ruleset_or_greedy
 from .inside_forest import (
     InsideForest,
     InsideForestClassifier,
@@ -52,6 +53,9 @@ __all__ = [
     "InsideForestRegressor",
     "Region",
     "Rule",
+    "find_comb_dim_spaces",
+    "plot_rule_metrics",
+    "select_ruleset_or_greedy",
 ]
 
 __version__ = "0.1.3"
